64ae232c3db949cf73dded63736baf6079b582b4
Debug1995 <33335167+Debug1995@users.noreply.github.com>
2018-11-27 18:22:28 -0500

    update some cases

--- unit_test.py
+++ unit_test.py
@@ -19,24 +19,30 @@ class Test(unittest.TestCase):
         self.assertIsNotNone(event_id_1)
         self.assertIsNotNone(EventController.add_event(self.user_1, invalid_event_1))
     
-    def test_retrieve_event(self):
-        # UserController.delete_user('nickname_1', 'email_1')
-        # UserController.add_user(self.user_1)
-        # user_2 = UserController.retrieve_user('email', 'email_1')
+    # def test_retrieve_event(self):
+    #     # UserController.delete_user('nickname_1', 'email_1')
+    #     # UserController.add_user(self.user_1)
+    #     # user_2 = UserController.retrieve_user('email', 'email_1')
 
-        # event_id = EventController.add_event(self.user_2, self.event_1)
-        # self.event_1.eventid = event_id
-        # EventController.host_event(user_2, self.event_1)
+    #     # event_id = EventController.add_event(self.user_2, self.event_1)
+    #     # self.event_1.eventid = event_id
+    #     # EventController.host_event(user_2, self.event_1)
 
         event_2 = EventController.retrieve_event('1')
         self.assertIsInstance(event_2, EventModel)
+        self.assertEqual(EventController.retrieve_event('-1'), 'MISSING')
 
-        UserController.delete_user('nickname_1', 'email_1')
+        # UserController.delete_user('nickname_1', 'email_1')
 
     def test_edit_event(self):
         event_id = EventController.add_event(self.user_1, self.event_1)
         event_2 = EventModel(1, 'event_title_2', 'tags_2', 'description_2', 'image_2', [], [], self.date, 'location_2', '5')
+        event_2.image = 'image_3'
+        self.assertEqual(EventController.edit_event(event_2), 'SUCCESS')
+        event_2.image = 'image_2'
+        self.assertEqual(EventController.edit_event(event_2), 'SUCCESS')        
         self.assertEqual(EventController.edit_event(event_2), 'MISSING')
+
     
 #     def test_expire:
         
@@ -71,7 +77,7 @@ class Test(unittest.TestCase):
     	self.assertEqual(UserController.delete_user('nickname_2', 'email_2'), 'SUCCESS')
     	self.assertEqual(UserController.delete_user('fake_nickname_1', 'fake_email_1'), 'SUCCESS')
         
-    def test_edit_user(self):
+    def test_edit_user_1(self):
         UserController.delete_user('nickname_3', 'email_2')
         UserController.delete_user('nickname_2', 'email_2')
         UserController.add_user(self.user_2)
@@ -81,25 +87,39 @@ class Test(unittest.TestCase):
         self.assertEqual(UserController.edit_user(user_3), 'MISSING')
         self.assertEqual(UserController.retrieve_user('email', 'email_2').nickname, 'nickname_3')
         UserController.delete_user('nickname_3', 'email_2')
+
+    def test_edit_user_2(self):
+        UserController.delete_user('nickname_3', 'email_2')
+        UserController.delete_user('nickname_2', 'email_2')
+        UserController.add_user(self.user_2)
+        user_3 = UserController.retrieve_user('email', 'email_2')
+        user_3.location = 'location_3'
+        self.assertIsNot(UserController.edit_user(user_3), 'MISSING' or 'DUPLICATE' or 'FAILURE')
+        self.assertEqual(UserController.edit_user(user_3), 'MISSING')
+        self.assertEqual(UserController.retrieve_user('email', 'email_2').location, 'location_3')
+        UserController.delete_user('nickname_3', 'email_2')
         
     def test_retrieve_user(self):
+        UserController.delete_user('nickname_1', 'email_1')
+        UserController.delete_user('nickname_2', 'email_2')
         UserController.add_user(self.user_1)
         self.assertIsInstance(UserController.retrieve_user('email', 'email_1'), UserModel)
         self.assertEqual(UserController.retrieve_user('email', 'email_2'), 'MISSING')
         UserController.delete_user('nickname_1', 'email_1')
         
 if __name__ == '__main__':
-    print(1)
-    log_file = 'report/unittest_log.txt'
-    f = open(log_file, 'a')
-    suite = unittest.TestSuite()
-    tests = [Test("test_add_event"), Test("test_retrieve_event"), Test("test_edit_event"), 
-             Test("test_add_user"), Test("test_delete_user"), 
-             Test("test_edit_user"), Test("test_retrieve_user")]
-    suite.addTests(tests)
-    runner = unittest.TextTestRunner(f)
-    runner.run(suite)
-    f.close()
-    print(2)
+    # print(1)
+    # log_file = 'report/unittest_log.txt'
+    # f = open(log_file, 'a')
+    # suite = unittest.TestSuite()
+    # tests = [Test("test_add_event"), Test("test_retrieve_event"), Test("test_edit_event"), 
+    #          Test("test_add_user"), Test("test_delete_user"), 
+    #          Test("test_edit_user_1"), Test("test_edit_user_2"), 
+    #          Test("test_retrieve_user")]
+    # suite.addTests(tests)
+    # runner = unittest.TextTestRunner(f)
+    # runner.run(suite)
+    # f.close()
+    # print(2)
     # unittest.main(testRunner=runner)
-    # unittest.main()
+    unittest.main()
